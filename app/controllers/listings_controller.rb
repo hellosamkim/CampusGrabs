9faa5e6edd5fb9058cd47168858d638ab1ca6fa7
hellosamkim <sam.mt.kim@gmail.com>
2015-02-16 11:58:31 -0500

    Generated listings controller, views, and routes

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,43 @@
+class ListingsController < ApplicationController
+  before_action :set_listing, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def edit
+  end
+
+  def update
+    if @listing.update_attributes(listing_params)
+      redirect_to @listing
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    if @listing.destroy
+      flash[:success] = "Listing was deleted"
+    else
+      flash[:error] = "Listing could not be deleted, please try again later."
+    end
+    redirect_to @listing
+  end
+
+  private
+  def set_listing
+    @listing = Listing.find(params[:id])
+  end
+
+  def listing_params
+    params.require(:listing).permit(:name, :descriptiohn, :price_in_cents)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  root 'listings#index'
+  resources :listings
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
