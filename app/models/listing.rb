57fec6320a1bbb705a0cf2d3f1b71208f3138efe
hellosamkim <sam.mt.kim@gmail.com>
2015-02-18 12:31:45 -0500

    Created associations between listings and users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
   validates :title, :description, presence: true
   validates :price, numericality: {only_integer: true}
+
+  belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218165742) do
+ActiveRecord::Schema.define(version: 20150218171703) do
 
   create_table "listings", force: true do |t|
     t.string   "title"
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20150218165742) do
     t.integer  "price"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "listings", ["user_id"], name: "index_listings_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
