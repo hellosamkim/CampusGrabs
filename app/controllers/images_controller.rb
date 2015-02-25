e197cd7de9f16ae330e2e105a105999cea66c5ee
hellosamkim <sam.mt.kim@gmail.com>
2015-02-25 14:10:25 -0500

    Users can fully add/ edit/ delete multiple pictures for each products

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -1,6 +1,6 @@
 class ImagesController < ApplicationController
-  before_action :set_image, only: [:show, :edit, :update, :destroy]
   before_action :set_listing
+  before_action :set_image, only: [:show, :edit, :update, :destroy]
   before_filter :ensure_logged_in, only: [:create, :edit, :destroy]
 
   def show
