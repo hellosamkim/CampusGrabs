a4d1e074cf791780506c5923f8dd4dd282786a00
hellosamkim <sam.mt.kim@gmail.com>
2015-02-28 13:11:08 -0500

    Add username to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-         
+
+  validates_presence_of :user_name         
+
   has_many :listings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227162347) do
+ActiveRecord::Schema.define(version: 20150228181053) do
 
   create_table "images", force: true do |t|
     t.integer  "listing_id"
@@ -44,9 +44,11 @@ ActiveRecord::Schema.define(version: 20150227162347) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["username"], name: "index_users_on_username", unique: true
 
 end
