89cf665cf949bfac3935ef4a1b456056cb5a4f86
hellosamkim <sam.mt.kim@gmail.com>
2015-02-28 23:39:55 -0500

    Added 50 users to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+50.times do
+  User.create(username: Faker::Internet.user_name, email: Faker::Internet.safe_email, password: "password", password_confirmation: "password")
+end
