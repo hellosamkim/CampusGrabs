968e55bcfd3a55380f89497dcc5aecfcc72ada20
hellosamkim <sam.mt.kim@gmail.com>
2015-02-28 23:49:53 -0500

    Populated site with Listings

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,9 @@
 50.times do
   User.create(username: Faker::Internet.user_name, email: Faker::Internet.safe_email, password: "password", password_confirmation: "password")
 end
+
+25.times do
+  Listing.create(title: Faker::Commerce.product_name, description: Faker::Lorem.paragraph(6), price: rand(1..500), user_id: rand(1..50))
+end
+
+
